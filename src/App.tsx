1ae01a3b708e99c5212a75876db16787d1a67631
Jerson <solorsanolopez@gmail.com>
2022-03-23 16:12:38 -0700

    [Add] default props [Specify] ReactEelement

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactElement, ReactNode } from 'react';
 import './App.css';
 
 // conventional props
@@ -6,15 +6,41 @@ function Heading({ title }: { title?: string }) {
   return <h1>{title}</h1>
 }
 
-function HeadingWithContent({ children }: { children?: string }) {
+
+// ReactNode will help to specify that the children can be any dataType 
+// With ReactElement we are specifing what we're going to return, in this case is a react element
+function HeadingWithContent({ children }: { children?: ReactNode }): ReactElement {
   return <h1>{children}</h1>
 }
+
+// deafultProps
+const defaultContainerProps = {
+  heading: <strong>My Heading</strong>
+}
+
+type ContainerProps = { children?: ReactNode } & typeof defaultContainerProps
+
+function Container({ heading, children }: ContainerProps): ReactElement {
+  return <div>
+    <h1>{heading}</h1>
+    {children}
+  </div>
+}
+
+Container.defaultProps = defaultContainerProps;
 
 function App() {
   return (
     <div>
       <Heading title="Hello world" />
-      <HeadingWithContent>Hi!</HeadingWithContent>
+      <HeadingWithContent>
+        <strong>
+          Hi!
+        </strong>
+      </HeadingWithContent>
+      <Container>
+        Foo
+      </Container>
     </div>
   );
 }
