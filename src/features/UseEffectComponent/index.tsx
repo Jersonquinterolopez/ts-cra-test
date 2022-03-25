9de43f73ad691dd3aca51da4ef5f613aa9ebd60e
Jerson <solorsanolopez@gmail.com>
2022-03-24 20:05:03 -0700

    [Complete] UseEffect chapter

--- src/features/UseEffectComponent/index.tsx
+++ src/features/UseEffectComponent/index.tsx
@@ -1,12 +1,19 @@
 import { useEffect, useState } from "react"
 
+// setInterval execute a function every none depending the time we specify
+// when we use valSet in this way:
+// valSet(v => v + 1) valSet is gonna get access to the scope of the useState and 
+// it's previous definition
+
 const UseEffectComponent = () => {
     const [val, valSet] = useState(1)
 
     useEffect(() => {
-        window.setInterval(() => {
+        const timer = window.setInterval(() => {
             valSet(v => v + 1)
         }, 1000)
+
+        return () => window.clearInterval(timer)
     }, []);
 
     return <div>{val}</div>
